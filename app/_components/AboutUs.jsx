040eb7c42a81181088655203852a152ffcf6b719
omar-farha <farha.omar2008@gmail.com>
2025-07-27 02:53:29 +0300

    Update AboutUs.jsx

--- app/_components/AboutUs.jsx
+++ app/_components/AboutUs.jsx
@@ -21,30 +21,30 @@ export function AnimatedTestimonialsDemo() {
       designation: "Web Developer",
       src: mariam,
     },
-    {
-      id: 3,
-      quote:
-        "Ne3ma isn’t just a platform; it’s a revolution. Our business workflow has become more streamlined, saving us both time and resources.",
-      name: "Marwan Elsabahy",
-      designation: "Business Developer",
-      src: marwan,
-    },
-    {
-      id: 4,
-      quote:
-        "I was blown away by the performance and reliability of Ne3ma. It’s rare to find a service that consistently delivers on its promises.",
-      name: "Kareem",
-      designation: "Frontend Developer",
-      src: "https://images.unsplash.com/photo-1636041293178-808a6762ab39?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 5,
-      quote:
-        "Ne3ma’s innovative approach has taken our projects to the next level. Whether you’re a startup or an enterprise, this is the solution you need!",
-      name: "Mohamed Elhadery",
-      designation: "Frontend Developer",
-      src: "https://images.unsplash.com/photo-1624561172888-ac93c696e10c?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
+    // {
+    //   id: 3,
+    //   quote:
+    //     "Ne3ma isn’t just a platform; it’s a revolution. Our business workflow has become more streamlined, saving us both time and resources.",
+    //   name: "Marwan Elsabahy",
+    //   designation: "Business Developer",
+    //   src: marwan,
+    // },
+    // {
+    //   id: 4,
+    //   quote:
+    //     "I was blown away by the performance and reliability of Ne3ma. It’s rare to find a service that consistently delivers on its promises.",
+    //   name: "Kareem",
+    //   designation: "Frontend Developer",
+    //   src: "https://images.unsplash.com/photo-1636041293178-808a6762ab39?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // },
+    // {
+    //   id: 5,
+    //   quote:
+    //     "Ne3ma’s innovative approach has taken our projects to the next level. Whether you’re a startup or an enterprise, this is the solution you need!",
+    //   name: "Mohamed Elhadery",
+    //   designation: "Frontend Developer",
+    //   src: "https://images.unsplash.com/photo-1624561172888-ac93c696e10c?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // },
   ];
   return <AnimatedTestimonials testimonials={testimonials} />;
 }
